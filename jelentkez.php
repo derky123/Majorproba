81dd525b83c658039ab828d72c93e911f6ab02dd
derky123 <bertoldbobak4@gmail.com>
2024-01-18 07:51:48 +0100

    Nagyonproba

---
+++ jelentkez.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="menu">
+        <ul>
+            <li><a class="" href="index.html">Home</a></li>
+            <li><a class="" href="gepek.html">Gépek</a></li>
+            <li><a class="active" href="jelentkez.php">Jelentkezz</a></li>
+            <li><a class="" href="elerhetoseg.html">Elérhetőség</a></li>
+        </ul>
+    </div>
+    <form action="asd.php" method="POST"> 
+    Vezetéknév: <input type="text" name="vez"/><br>
+    Keresztnév: <input type="text" name="ker"/><br>
+    Szüvetési dátum: <input type="text" name="valami"/><br>
+    Szüvetési hely: <input type="text" name="valami"/><br>
+    Telefonszám: <input type="text" name="valami"/><br>
+    Lakcím: <input type="text" name="valami"/><br>
+    Neme: Nő <input type="radio" name="valami"/> Férfi <input type="radio" name="valami"/><br>
+    <input type="Submit" name="submit" value="Submit">
+</form>
+</body>
+</html>
